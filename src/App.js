00dc5d9ee750580b5407e1f9a28e5957fdfbe539
minho2618 <minho2618@naver.com>
2023-03-11 01:22:01 +0900

    2023-03-11 시작

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="text" placeholder="To-do" id="toDo" />
+        <input type="submit" value="추가" />
+      </form>
     </div>
   );
 }
